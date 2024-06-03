8e54771c0b07bbea98099d339cd49f19d09f1f4a
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-04 03:36:49 +0700

    update: DT(Date)

---
+++ src/test/java/belajar/date/time/core/DateTest.java
@@ -0,0 +1,17 @@
+package belajar.date.time.core;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class DateTest {
+
+    @Test
+    void testCreate() {
+        var date1 = new Date();
+        var date2 = new Date(System.currentTimeMillis());
+
+        System.out.println(date1);
+        System.out.println(date2);
+    }
+}
